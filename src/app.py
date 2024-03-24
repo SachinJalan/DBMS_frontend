daf81dde15454357d902e2166db796e039153b24
anish-karnik <114944809+anish-karnik@users.noreply.github.com>
2024-03-24 19:15:42 +0530

    Added Profile,Course Selection, Creation

--- src/app.py
+++ src/app.py
@@ -22,22 +22,6 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    # if request.method == 'POST':
-    #     details = request.form
-    #     email = session.get('email')
-    #     # Fetch form data
-    #     details = request.form
-    #     name = details['name']
-    #     lab_name = details['lab_name']
-    #     date_from = details['date_from']
-    #     date_to = details['date_to']
-    #     time = details['time']
-
-    #     # Insert into MySQL
-    #     cur = mysql.connection.cursor()
-    #     cur.execute("INSERT INTO bookings (name, lab_name, date_from, date_to, time) VALUES (%s, %s, %s, %s, %s)", (name, lab_name, date_from, date_to, time))
-    #     mysql.connection.commit()
-    #     cur.close()
         
     if request.method == 'POST':
         details = request.form
@@ -56,28 +40,79 @@ def submit():
                 cur.execute("INSERT INTO bookings (user_email,name,lab_name,time_from, time_to,date) VALUES (%s,%s, %s, %s, %s, %s)", (email,name, lab_name, time_from, time_to,date))
                 mysql.connection.commit()
                 cur.close()
-                          
+        
+        elif 'Equipment_Name' in details:
+                # Equipment issuing form data
+                equipment_name = details['Equipment_Name']
+                equipment_id= details['ID']
+                quantity = details['Quantity']
+                price= details['Price']
+                vendor_address= details['Vendor_Address']
+                vendor_phone_number= details['Vendor_Phone_Number']
+                manufacturer_name= details['Manufacturer_Name']
+                status= details['Status']
+                lab_name= details['Lab_Name']
+                
+                cur = mysql.connection.cursor()
+                cur.execute("INSERT INTO inventory ( ID,Equipment_Name, Quantity, Price, Vendor_Address, Vendor_Phone_Number, Manufacturer_Name,  Status ,  Lab_Name )  VALUES (%s, %s,%s, %s, %s, %s, %s, %s,%s)", (equipment_id,equipment_name, quantity, price, vendor_address, vendor_phone_number, manufacturer_name, status, lab_name))
+                mysql.connection.commit()
+                cur.close()
+        
+        elif 'Enrolled_Course_ID' in details:
+                enrolled_course_id= details['Enrolled_Course_ID'][1:-1]                
+                enrolled_course_id=enrolled_course_id.split(',')
+                for i in range(len(enrolled_course_id)):
+                    enrolled_course_id[i]=enrolled_course_id[i][1:-1].strip()
+            
+                cur = mysql.connection.cursor()
+                enrolled_course_id=enrolled_course_id[0]
+                cur.execute("INSERT INTO student_enrolled (Course_Id, email) VALUES (%s, %s)", (enrolled_course_id,email))
+                mysql.connection.commit()
+                cur.close()
+          
+        elif 'Course_Name' in details:
+            # print("Hello")
+            course_name= details['Course_Name']
+            course_id= details['Course_ID']
+            credits= details['Credits']
+            
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO course (email,Course_ID, Course_Name, Credits) VALUES (%s,%s, %s, %s)", (email,course_id,course_name, credits))
+            mysql.connection.commit()    
+            cur.close()
+                
         else:
                 # Equipment issuing form data
                 equipment_type = details['equipmentType']
                 number_of_equipment = details['numberOfEquipment']
                 issue_date = details['issueDate']
                 return_date = details['returnDate']
-
-                # Insert equipment issuing data into EquipmentIssued table
-                # sql = "INSERT INTO EquipmentIssued (user_email, equipment_type, number_of_equipment, issue_date, return_date) VALUES (%s, %s, %s, %s, %s)"
-                # val = (email, equipment_type, number_of_equipment, issue_date, return_date)
                 
                 cur = mysql.connection.cursor()
                 cur.execute("INSERT INTO EquipmentIssued (user_email, equipment_type, number_of_equipment, issue_date, return_date) VALUES (%s, %s, %s, %s, %s)", (email, equipment_type, number_of_equipment, issue_date, return_date))
                 mysql.connection.commit()
                 cur.close()
                 
-        # lab_bookings = fetch_lab_bookings(email)
         lab_bookings=fetch_lab_bookings(email)
         equipment_issued = fetch_equipment_issued(email)
-        return render_template('submit.html', equipment_issued=equipment_issued, lab_bookings=lab_bookings)
-        # return render_template('submit.html')
+        courses=fetch_courses()
+        return render_template('submit.html', equipment_issued=equipment_issued, lab_bookings=lab_bookings,courses=courses)
+
+@app.route('/profile')
+def profile():
+    email=session.get('email')
+    lab_bookings=fetch_lab_bookings(email)
+    equipment_issued = fetch_equipment_issued(email)
+    role=fetch_role(email)
+    name=fetch_name(email)
+    if(role=='student'):
+        courses=fetch_student_courses(email)
+        return render_template('profile.html', equipment_issued=equipment_issued, lab_bookings=lab_bookings,courses=courses,name=name,role = role)
+    elif(role=='professor'):
+        courses=fetch_prof_course(email)
+        return render_template('profile.html', lab_bookings=lab_bookings,courses=courses,name=name,role = role)
+    else:
+        return render_template('profile.html',name=name,role =role)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -101,27 +136,7 @@ def login():
         else:
             # Redirect to login page with error message if login fails
             return render_template('login.html', error="Invalid email or password")
-        
-        
-        mesage = ''
-    # if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-    #     email = request.form['email']
-    #     password = request.form['password']
-    #     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    #     cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password, ))
-    #     user = cursor.fetchone()
-    #     if user:
-    #         session['loggedin'] = True
-    #         session['userid'] = user['id']
-    #         session['name'] = user['first_name']
-    #         session['email'] = user['email']
-    #         session['role'] = user['role']
-    #         mesage = 'Logged in successfully !'            
-    #         return redirect(url_for('dashboard'))
-    #     else:
-    #         mesage = 'Please enter correct email / password !'
-    # return render_template('login.html', mesage = mesage)
-    
+            
     if request.method == 'GET':
         return render_template('login.html')
     
@@ -133,10 +148,12 @@ def register():
     if request.method == 'POST':
         email = request.form['reg_email']
         password = request.form['reg_password']
+        role = request.form['role']
+        name = request.form['name']
 
         # Insert new user into database
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users (email, password) VALUES (%s, %s)", (email, password))
+        cur.execute("INSERT INTO users (email, name,password,role) VALUES (%s, %s, %s,%s)", (email, name,password,role))
         mysql.connection.commit()
         cur.close()
 
@@ -149,7 +166,10 @@ def register():
 @app.route('/bookinglab')
 def booking_lab():
     if 'loggedin' in session:
-        return render_template('bookinglab.html')
+        role=fetch_role(session.get('email'))
+        courses=fetch_courses()
+        # print(role)
+        return render_template('bookinglab.html', role=role,courses=courses)
     return redirect(url_for('login'))
 
 def fetch_lab_bookings(email):
@@ -166,16 +186,39 @@ def fetch_equipment_issued(email):
     
     return equipment_issued
 
-# @app.route('/submit')
-# def new_submit():
-#     # Fetch lab bookings and equipment issued for the current user
+def fetch_role(email):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT role FROM users WHERE email = %s", (email,))
+    role = cur.fetchone() 
     
-#     email=session.get('email')  # Assuming you have a function to get the current user's ID
-#     lab_bookings = fetch_lab_bookings(email)
-#     equipment_issued = fetch_equipment_issued(email)
-    
-#     return render_template('submit.html', lab_bookings=lab_bookings, equipment_issued=equipment_issued)
+    return role[0]
 
+def fetch_courses():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT Course_ID FROM course")
+
+    courses = cur.fetchall()  # Fetch all rows
+    # print(courses)
+    return courses
+
+def fetch_student_courses(email):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT Course_ID FROM student_enrolled WHERE email = %s", (email,))
+    courses = cur.fetchall()  # Fetch all rows
+    return courses
+
+def fetch_prof_course(email):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT Course_ID FROM course WHERE email = %s", (email,))
+    courses = cur.fetchall()  # Fetch all rows
+    return courses
+
+def fetch_name(email):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT name FROM users WHERE email = %s", (email,))
+    name = cur.fetchone() 
+    
+    return name[0]
 
 
 if __name__ == '__main__':
--- src/bookings.sql
+++ src/bookings.sql
@@ -12,12 +12,44 @@ CREATE TABLE IF NOT EXISTS bookings (
     date DATE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS users (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `password` VARCHAR(255) NOT NULL
+
+CREATE TABLE IF NOT EXISTS `inventory` (
+  `ID` bigint NOT NULL,
+  `Equipment_Name` varchar(255) NOT NULL,
+  `Price` float DEFAULT NULL,
+  `Vendor_Address` varchar(255) DEFAULT NULL,
+  `Vendor_Phone_Number` varchar(255) DEFAULT NULL,
+  `Manufacturer_Name` varchar(255) DEFAULT NULL,
+  `Status` varchar(255) DEFAULT NULL,
+    `Lab_Name` ENUM('Anatomy Lab', 'Biochemistry Lab', 'Biology Lab', 'Botany Lab', 'Chemistry Lab', 'Computer Lab', 'Ecology Lab', 'Engineering Lab', 'Genetics Lab', 'Geology Lab', 'Microbiology Lab', 'Neuroscience Lab', 'Physics Lab', 'Psychology Lab', 'Zoology Lab') NOT NULL, 
+  `Quantity` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID` (`ID`)
 );
 
+CREATE TABLE IF NOT EXISTS users (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    `role` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `course` (
+	`email` varchar(255) NOT NULL,
+  `Course_ID` varchar(255) NOT NULL,
+  `Credits` int DEFAULT NULL,
+  `Course_Name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Course_ID`),
+  UNIQUE KEY `Course_ID` (`Course_ID`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `student_enrolled` (
+  `Course_ID` varchar(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`Course_ID`,`email`)
+);
 
 CREATE TABLE IF NOT EXISTS EquipmentIssued (
     id INT AUTO_INCREMENT PRIMARY KEY,
